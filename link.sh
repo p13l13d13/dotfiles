f124a0b80759833b968d918fad76bad10c675c56
Ivan Gulakov <gulakov@ispras.ru>
2020-07-19 22:46:17 +0300

    Move from nvim to vim

--- link.sh
+++ link.sh
@@ -4,6 +4,7 @@ ln -sf $(pwd)/config/i3/config $HOME/.config/i3/config
 ln -sf $(pwd)/config/alacritty/alacritty.yml $HOME/.config/alacritty/alacritty.yml  
 ln -sf $(pwd)/config/nvim/init.vim $HOME/.config/nvim/nvim.init  
 
+ln -sf $(pwd)/dotfiles/.vimrc $HOME/.vimrc
 ln -sf $(pwd)/dotfiles/.gitconfig   $HOME/.gitconfig  
 ln -sf $(pwd)/dotfiles/.gitglobalignore $HOME/.gitglobalignore  
 ln -sf $(pwd)/dotfiles/.profile $HOME/.profile  
