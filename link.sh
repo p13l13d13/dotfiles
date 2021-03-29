ec41fe4103188977e57d8e410e70dc4caa3d127d
Ivan Gulakov <gulakov@ispras.ru>
2021-03-29 16:46:27 +0300

    Update i3 conf and add missing i3blocks config

--- link.sh
+++ link.sh
@@ -3,6 +3,7 @@
 ln -sf $(pwd)/config/i3/config $HOME/.config/i3/config   
 ln -sf $(pwd)/config/alacritty/alacritty.yml $HOME/.config/alacritty/alacritty.yml  
 ln -sf $(pwd)/config/nvim/init.vim $HOME/.config/nvim/nvim.init  
+ln -sf $(pwd)/config/i3/i3blocks.conf $HOME/.config/i3/i3blocks.conf
 
 ln -sf $(pwd)/dotfiles/.vimrc $HOME/.vimrc
 ln -sf $(pwd)/dotfiles/.gitconfig   $HOME/.gitconfig  
