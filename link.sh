77debb94f40b2dea5c5d8478b86c073ca77380df
Ivan Gulakov <gulakov@amazon.de>
2025-06-30 02:57:34 +0200

    Fix incorrect paths and typos

--- link.sh
+++ link.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-mkdir -p $HOME/config/alacritty
-mkdir -p $HOME/config/wallpaper
-mkdir -p $HOME/config/sway
-mkdir -p $HOME/config/ranger
-mkdir -p $HOME/config/hypr
-mkdir -p $HOME/config/quickshell
+mkdir -p $HOME/.config/alacritty
+mkdir -p $HOME/.config/wallpaper
+mkdir -p $HOME/.config/sway
+mkdir -p $HOME/.config/ranger
+mkdir -p $HOME/.config/hypr
+mkdir -p $HOME/.config/quickshell
 
 ln -sf $(pwd)/config/quickshell $HOME/.config/quickshell
 ln -sf $(pwd)/config/hypr $HOME/.config/hypr
@@ -14,10 +14,10 @@ ln -sf $(pwd)/config/alacritty/alacritty.yml $HOME/.config/alacritty/alacritty.y
 ln -sf $(pwd)/config/helix/config.toml $HOME/.config/helix/config.toml 
 ln -sf $(pwd)/config/helix/languages.toml $HOME/.config/helix/languages.toml
 
-ln -sf $(pwd)/home_dotfiles/.gitconfig   $HOME/.gitconfig  
-ln -sf $(pwd)/home_dotfiles/.gitglobalignore $HOME/.gitglobalignore  
-ln -sf $(pwd)/home_dotfiles/.profile $HOME/.profile  
-ln -sf $(pwd)/home_dotfiles/.Xresources $HOME/.Xresources  
-ln -sf $(pwd)/home_dotfiles/.zshrc $HOME/.zshrc  
+ln -sf $(pwd)/dotfiles/.gitconfig   $HOME/.gitconfig
+ln -sf $(pwd)/dotfiles/.gitglobalignore $HOME/.gitglobalignore
+ln -sf $(pwd)/dotfiles/.profile $HOME/.profile
+ln -sf $(pwd)/dotfiles/.Xresources $HOME/.Xresources
+ln -sf $(pwd)/dotfiles/.zshrc $HOME/.zshrc
 
 ln -sf $(pwd)/config/wallpaper/wallpaper.jpg $HOME/.config/wallpaper/wallpaper.jpg
