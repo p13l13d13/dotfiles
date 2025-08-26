d13cc82ad6e82de2e80c2bb016e9ae7f92a472d2
Ivan Gulakov <gulakov@protonmail.com>
2025-08-27 00:01:54 +0200

    update hyprland configs

--- link.sh
+++ link.sh
@@ -21,3 +21,4 @@ done
 
 ln -sfn "$repo_dir/config/fish/config.fish" "$HOME/.config/fish"
 ln -sfn -t "$HOME" "$repo_dir"/dotfiles/.[!.]* "$repo_dir"/dotfiles/..?*
+cp ./config/wallpaper/wallpaper.jpg "$HOME/current.wall"
